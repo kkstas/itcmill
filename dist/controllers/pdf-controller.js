29a55c966b9112db1fa8b6c43bbe3e8f99cd2237
kkstas <109525588+kkstas@users.noreply.github.com>
2023-06-06 13:15:00 +0200

    Update pdf-controller.js

--- dist/controllers/pdf-controller.js
+++ dist/controllers/pdf-controller.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.pdfController = void 0;
 const pdf_service_1 = require("../service/pdf-service");
 const pdfController = (req, res, _next) => {
+    console.time();
     // const data = getExampleCashOutTrxData();
     const reqData = {
         transactionStartDateTime: `${req.query.a}`,
@@ -29,6 +30,7 @@ const pdfController = (req, res, _next) => {
     });
     (0, pdf_service_1.buildCashOutPDF)(reqData, // dane do pokwitowania
     (chunk) => stream.write(chunk), () => stream.end());
+    console.timeEnd();
 };
 exports.pdfController = pdfController;
 // const queryString = "?a=12:24:43&b=RNET6338&c=asldkfjwod fw&d=ITCARD&e=ul. Zwycięska 43&f=Wrocław&g=234234*******3242342&h=200,00 PLN";
